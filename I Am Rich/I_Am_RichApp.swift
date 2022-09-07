72d3a0372fbb08ea324ac1881f774d5bc766de14
Pack <packaq3@gmail.com>
2022-09-07 13:02:35 +0700

    Initial Commit

---
+++ I Am Rich/I_Am_RichApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_RichApp.swift
+//  I Am Rich
+//
+//  Created by Pipat on 7/9/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_RichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
